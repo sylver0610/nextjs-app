55d984fd3aa3957e96bfeb0461f8be3b40f68d70
sylver0610 <79132069+sylver0610@users.noreply.github.com>
2023-05-04 01:33:36 +0300

    template nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '@component/styles/globals.css'
+import '../styles/reset.css'
+import '@nextcss/reset'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
